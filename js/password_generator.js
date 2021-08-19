fec8ce01a11fa0152f9bfbb1a4e13859795138e4
Joshua Palladeno Hamilton <75545201+Jpalladeno1@users.noreply.github.com>
2021-08-19 10:20:52 -0500

    another commit

--- js/password_generator.js
+++ js/password_generator.js
@@ -1,1 +1,39 @@
-// comment comment//
+//Dom Elements
+//const resultEL = document.getElementById('result');
+const lengthEL = document.getElementById('length');
+const uppercaseEL = document.getElementById('uppercase');
+const lowercaseEL = document.getElementById('lowercase');
+const numEL = document.getElementById('num');
+const symbolsEL = document.getElementById('symbols');
+const generateEL = document.getElementById('generate');
+const clipboardEL = document.getElementById('clipboard');
+
+const randomFunc = {
+    lower: getRandomLower, 
+    upper: getRandomUpper,
+    number: getRandomNum,
+    symbol: getRandomSymbol
+}; 
+
+generateEL.addEventListener('click', () => {
+    const length = lengthEL.value;
+    console.log(length);
+})
+// Generator Functions
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getRandomNum() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function getRandomSymbol() {
+    const symbols = "!@#$%^&*?,.";
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
+
+console.log(getRandomSymbol())
